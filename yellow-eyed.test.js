467e8042488d4c81f0ee76af1641cb10d9497e4f
fumi23 <fmr23s@gmail.com>
2021-03-12 03:34:57 +0900

    テスト全部通る！

--- yellow-eyed.test.js
+++ yellow-eyed.test.js
@@ -1,16 +1,18 @@
-const response = jest.fn()
 const { createServer } = require('net')
-createServer(conn => {
-  let buffer = ''
-  conn.on('data', data => {
-    buffer += data.toString()
-    const pos = buffer.indexOf('\r\n')
-    if (pos === -1) return
 
-    buffer = ''
-    conn.write(response() + '\r\n')
-  })
-}).listen(51013)
+function createMockServer(response) {
+  return createServer(conn => {
+    let buffer = ''
+    conn.on('data', data => {
+      buffer += data.toString()
+      const pos = buffer.indexOf('\r\n')
+      if (pos === -1) return
+
+        buffer = ''
+        conn.write(response + '\r\n')
+    })
+  }).listen(51013)
+}
 
 
 const YellowEyed = require('yellow-eyed')
@@ -18,7 +20,7 @@ const YellowEyed = require('yellow-eyed')
 const client = new YellowEyed('127.0.0.1')
 
 test('reeceive ok response', async () => {
-  response.mockReturnValueOnce('se;ok;74.00;41.08;17.17')
+  const server = createMockServer('se;ok;74.00;41.08;17.17')
 
   const promise = client.getAllSensorValue()
   await expect(promise).resolves.toEqual({
@@ -26,25 +28,29 @@ test('reeceive ok response', async () => {
     humidity: 41.08,
     temperature: 17.17
   })
+  server.close()
 })
 
 test('reeceive error response', async () => {
-  response.mockReturnValueOnce('se;err;001')
+  const server = createMockServer('se;err;001')
 
   const promise = client.getAllSensorValue()
   await expect(promise).rejects.toThrow('Error Code: 001')
+  server.close()
 })
 
 test('reeceive unknown response', async () => {
-  response.mockReturnValueOnce('se;yyy')
+  const server = createMockServer('se;yyy')
 
   const promise = client.getAllSensorValue()
   await expect(promise).rejects.toThrow('Unknown result')
+  server.close()
 })
 
 test('reeceive mismatched response', async () => {
-  response.mockReturnValueOnce('xx:ok')
+  const server = createMockServer('xx:ok')
 
   const promise = client.getAllSensorValue()
   await expect(promise).rejects.toThrow('Command mismatched')
+  server.close()
 })
