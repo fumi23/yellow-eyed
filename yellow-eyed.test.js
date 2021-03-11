444808cd484f82e063caf2cc8ac7dbdffe5090a9
fumi23 <fmr23s@gmail.com>
2021-03-12 04:57:46 +0900

    テストを追加 (#11)
    * npm install --save-dev jest

---
+++ yellow-eyed.test.js
@@ -0,0 +1,41 @@
+const YellowEyed = require('yellow-eyed')
+
+const client = new YellowEyed('127.0.0.1')
+
+function mockResponse(message) {
+  jest
+    .spyOn(client, 'sendCommand')
+    .mockReturnValue(Promise.resolve(message))
+}
+
+describe('getAllSensorValue', () => {
+  const subject = () => client.getAllSensorValue()
+
+  test('receive ok response', async () => {
+    mockResponse('se;ok;74.00;41.08;17.17')
+
+    await expect(subject()).resolves.toEqual({
+      illuminance: 74,
+      humidity: 41.08,
+      temperature: 17.17
+    })
+  })
+
+  test('receive error response', async () => {
+    mockResponse('se;err;001')
+
+    await expect(subject()).rejects.toThrow('Error Code: 001')
+  })
+
+  test('receive unknown response', async () => {
+    mockResponse('se;yyy')
+
+    await expect(subject()).rejects.toThrow('Unknown result')
+  })
+
+  test('receive mismatched response', async () => {
+    mockResponse('xx:ok')
+
+    await expect(subject()).rejects.toThrow('Command mismatched')
+  })
+})
