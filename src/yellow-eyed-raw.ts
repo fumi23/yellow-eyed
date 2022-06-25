2605da6c3d877a9a634d96cdca3f995af90f2151
fumi23 <fmr23s@gmail.com>
2022-06-25 13:10:54 +0900

    async-generator

--- src/yellow-eyed-raw.ts
+++ src/yellow-eyed-raw.ts
@@ -1,40 +1,68 @@
-import { connect } from 'net'
+import { Socket } from 'net'
 
 const PORT = 51013
 
-export type Callback = (_response: string) => void
-
 export default class YellowEyedRaw {
-  private client: ReturnType<typeof connect>
-  private callback = (_response: string) => {}
+  private host: string
+  private socket: Socket
+  private onConnect = () => {}
+  private onError = (_error: Error) => {}
+  private onData = (_data: Buffer) => {}
+  private onEnd = () => {}
+  private onClose = (_hadError: boolean) => {}
 
   constructor(host: string) {
+    this.host = host
+    this.socket = new Socket()
+      .on('connect', () => this.onConnect())
+      .on('error', (error: Error) => this.onError(error))
+      .on('data', (data: Buffer) => this.onData(data))
+      .on('end', () => this.onEnd())
+      .on('close', (hadError: boolean) => this.onClose(hadError))
+  }
+
+  connect(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.onConnect = resolve // 接続が正常に確立したとき
+      this.onError = reject // 接続できなかったとき
+      this.socket.connect(PORT, this.host)
+    })
+  }
+
+  async *waitReceive(): AsyncGenerator<string> {
     let buffer = ''
-    this.client = connect(PORT, host)
-    this.client.on('data', data => {
-      // CRLF を受信するまでバッファリング
-      buffer += data.toString()
+    for (;;) {
+      const data = await new Promise<Buffer | void>((resolve, reject) => {
+        this.onData = resolve // データを受信したとき
+        this.onEnd = resolve // EOT を受信したとき (= Passive Close)
+        this.onError = reject // 通信中のエラーが発生したとき
+      })
+      if (data) {
+        buffer += data.toString()
+      } else {
+        break
+      }
       for (;;) {
+        // CRLF を受信するまでバッファリング
         const pos = buffer.indexOf('\r\n')
         if (pos === -1) {
           break
         }
         const response = buffer.substring(0, pos)
-        this.callback(response)
         buffer = buffer.substring(pos + 2)
+        yield response
       }
-    })
-  }
-
-  onRecv(callback: Callback): void {
-    this.callback = callback
+    }
   }
 
   send(command: string): void {
-    this.client.write(`*${command}\r\n`)
+    this.socket.write(`*${command}\r\n`)
   }
 
-  close(): void {
-    this.client.end()
+  close(): Promise<boolean> {
+    return new Promise(resolve => {
+      this.onClose = resolve // fully closed になったとき
+      this.socket.end()
+    })
   }
 }
